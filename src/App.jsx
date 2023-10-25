893daa8bc87f0f4691c24b2114040a3bc7259812
Ildi <128381088+ildizsigrai@users.noreply.github.com>
2023-10-25 11:50:23 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import CssBaseLine from "@mui/material/CssBaseline";
 import TodoList from './TodoList';
@@ -9,7 +7,6 @@ import NavBar from './NavBar';
 function App() {
   return (
     <>
-
       <CssBaseLine/>
       <NavBar/>
       <TodoList/>
